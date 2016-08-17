100dd4edd0c628be4a0cb7166c47193ed90c3a72
Yuchao <459723979@qq.com>
2016-08-16 18:05:28 -0700

    pokeYuchao init

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PokeYuchao
+//
+//  Created by 高宇超 on 8/2/16.
+//  Copyright © 2016 Yuchao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
